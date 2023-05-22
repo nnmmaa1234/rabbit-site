8f8d4f1f67a48c44f4b1f22eac587a69d597caad
Nooriya <57182904+nnmmaa1234@users.noreply.github.com>
2023-05-22 09:57:59 +0500

    Update settings.py

--- RooftopRabbitry/settings.py
+++ RooftopRabbitry/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rooftop-rabbitry.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nnmmaa9876.pythonanywhere.com']
 
 
 # Application definition
